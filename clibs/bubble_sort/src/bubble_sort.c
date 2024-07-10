a6f50375fbfd1a466706eb4a6e8a8e544dffdaeb
konekotech <89322889+konekotech@users.noreply.github.com>
2024-07-10 23:52:39 +0900

    add README

--- clibs/bubble_sort/src/bubble_sort.c
+++ clibs/bubble_sort/src/bubble_sort.c
@@ -2,11 +2,11 @@
 void bubble_sort(int *array, int size) {
     int i, j, tmp;
     for (i = 0; i < size - 1; i++) {
-        for (j = size - 1; j > i; j--) {
-            if (array[j - 1] > array[j]) {
+        for (j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
                 tmp = array[j];
-                array[j] = array[j - 1];
-                array[j - 1] = tmp;
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
             }
         }
     }
