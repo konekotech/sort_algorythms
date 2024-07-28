a8829886ac680745bc69f2962b7efce522513c0f
houbarurisu <f2110547@gl.cc.uec.ac.jp>
2024-07-28 16:07:33 +0900

    add: C implementation of merge sort

---
+++ clibs/merge_sort/include/lib.h
@@ -0,0 +1,6 @@
+#ifndef MERGE_SORT_LIB_H
+#define MERGE_SORT_LIB_H
+
+void sort(int *array, int size);
+
+#endif //MERGE_SORT_LIB_H
---
+++ clibs/merge_sort/src/lib.c
@@ -0,0 +1,7 @@
+#include "lib.h"
+#include "merge_sort.h"
+
+// Sort an array
+void sort(int *array, int size) {
+    merge_sort(array, size);
+}
